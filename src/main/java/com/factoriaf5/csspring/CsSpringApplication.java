5d4f431c496fae0681b1dd18a0653d99a17f102f
pilpod <giacomo.corso@gmail.com>
2022-03-27 13:43:33 +0200

    init repo

---
+++ src/main/java/com/factoriaf5/csspring/CsSpringApplication.java
@@ -0,0 +1,13 @@
+package com.factoriaf5.csspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/factoriaf5/csspring/CsSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.factoriaf5.csspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
